249b9f71a0b95ffc54019d1a6d1b4fe0fd42e526
Christy Perozzi <christy.perozzi@gmail.com>
2022-01-09 16:01:24 -0500

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  
+  const [searchTerm, setSearchTerm] = useState("")
+  
+  function handleSearch(searched){
+    setSearchTerm(searched);
+    console.log("from App.js: " + searchTerm)
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={handleSearch} />
+      <ListingsContainer searchTerm={searchTerm} />
     </div>
   );
 }
 
 export default App;
+
+/*
+
+App
+  |
+  |---Header
+  |    |---Search
+  |
+  |---ListingsContainer 
+      |--- ListingCard
+
+For the 4th deliverable, pass the search term up from Search to App to be used down in the listings 
+*/
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,10 +1,26 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useState, useEffect} from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({searchTerm}) {
+
+  const [listings, setListings] = useState([])
+
+  useEffect( ()=> {fetch('http://localhost:6001/listings')
+                  .then(resp => resp.json())
+                  .then( data => setListings(data))
+                }, []
+             )
+
+  function deleteFromState(item){
+    const filteredItems = listings.filter( (x) => x.id !== item.id)
+    setListings(filteredItems)
+  }
+  
   return (
     <main>
       <ul className="cards">
+        {listings.filter((listing)=> listing.description.toLowerCase().includes(searchTerm.toLowerCase())).map( (listing) => { 
+          return <ListingCard listing = {listing} key={listing.id} onDelete={deleteFromState}/>})}
         {/* use the ListingCard component to display listings */}
       </ul>
     </main>
